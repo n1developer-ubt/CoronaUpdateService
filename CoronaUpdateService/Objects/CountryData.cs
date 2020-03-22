8dc89f134489fa03d233038895d4301cc4ea09c4
Usama Tariq <usamat37@gmail.com>
2020-03-22 03:40:37 -0700

    Add project files.

---
+++ CoronaUpdateService/Objects/CountryData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace CoronaDataScraper
+{
+    
+    class CountryData
+    {
+        public string Name { get; set; }
+        public string TotalCases { get; set; }
+        public string NewCases { get; set; }
+        public string TotalDeaths { get; set; }
+        public string NewDeaths { get; set; }
+        public string TotalRecovered { get; set; }
+        public string ActiveCases { get; set; }
+        public string Serious { get; set; }
+        public string TotCases { get; set; }
+
+        public bool Load(HtmlNode row)
+        {
+            List<HtmlNode> col = row.Descendants().Where((d)=>d.Name == "td").ToList();
+
+            try
+            {
+                Name = col[0].SelectSingleNode("a").InnerText;
+            }
+            catch (Exception e)
+            {
+                Name = col[0].InnerText.Trim();
+            }
+
+            TotalCases= col[1].InnerText;
+            NewCases= col[2].InnerText;
+            TotalDeaths= col[3].InnerText;
+            NewDeaths= col[4].InnerText;
+            TotalRecovered= col[5].InnerText;
+            ActiveCases= col[6].InnerText;
+            Serious= col[7].InnerText;
+            TotCases= col[8].InnerText;
+
+            return true;
+        }
+
+        public void Print()
+        {
+            foreach (PropertyInfo property in this.GetType().GetProperties())
+            {
+                Console.WriteLine(property.Name + ":\t" + property.GetValue(this,null));
+            }
+            Console.WriteLine();
+            //Console.WriteLine($"Name: \t" +
+            //                  $"TotalCase: \t" +
+            //                  $"NewCase: \t" +
+            //                  $"TotalDeaths: \t" +
+            //                  $"NewDeaths: \t" +
+            //                  $"TotalRecovered: \t" +
+            //                  $"Active");
+        }
+    }
+}
---
+++ CoronaUpdateService/Objects/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace CoronaDataScraper
+{
+    class Program
+    {
+        private static string WebSiteToScrape = "https://www.worldometers.info/coronavirus/";
+        static void Main(string[] args)
+        {
+            //string source = WebSource(WebSiteToScrape);
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(File.ReadAllText("data.html"));
+
+            HtmlNode table = doc.DocumentNode.SelectSingleNode("//*[@id=\"main_table_countries_today\"]");
+
+            HtmlNode rows = table.SelectSingleNode("//tbody[1]");
+
+            List<HtmlNode> td = rows.Descendants().Where((e) => e.Name=="tr").ToList();
+
+            List<CountryData> data = new List<CountryData>();
+
+            foreach (HtmlNode node in td)
+            {
+                CountryData newData = new CountryData();
+                newData.Load(node);
+                data.Add(newData);
+                newData.Print();
+            }
+            Console.WriteLine(data.Count);
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string WebSource(string website)
+        {
+            string source = null;
+            using (WebClient client = new WebClient())
+            {
+                source = client.DownloadString(website);
+            }
+
+            return source;
+        }
+    }
+}
